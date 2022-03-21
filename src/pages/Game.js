5efc44cb35e2f9af9a7d0c3fe223e63d2118d17d
phillip-vo <philvo1991@gmail.com>
2022-03-21 17:20:06 -0500

    Implemented individual game page

--- src/pages/Game.js
+++ src/pages/Game.js
@@ -1,34 +1,62 @@
 import "./Game.css";
 
+import * as React from 'react';
 import Box from "../components/Box";
+
 import { useState, useEffect } from "react";
 
 import axios from "axios";
 
-function Game(props) {
-  const [width, updateWidth] = useState(window.innerWidth);
-  window.addEventListener("resize", changeSize);
+import { useParams, useLocation } from 'react-router-dom';
 
-  function changeSize() {
-    updateWidth(window.innerWidth);
-  }
+function Game(props) {
+
+  const [game, setGame] = useState({});
+
+  const { gameName } = useParams();
+
+  console.log("Props Paramter Value - " + gameName);
+
+  useEffect(() => {
+    let isApiSubscribed = true;
+
+    axios
+      .get(
+        "https://teamnathanielrevatureproject2.azurewebsites.net/gameName/" + gameName
+      )
+      .then((response) => {
+        if (isApiSubscribed) {
+          console.log(response);
+          setGame(response.data);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+
+    return () => {
+      isApiSubscribed = false;
+    };
+  }, []);
 
 
   return (
+
         <div className='container vh-100'>
 
-            <div className='row'>
+            <div className='row mt-5'>
                 <Box className='col-12 col-md-4'>
-                    <img src='https://upload.wikimedia.org/wikipedia/en/thumb/0/03/Super_Mario_Bros._box.png/220px-Super_Mario_Bros._box.png' className='character-image'/>
+                    <img src={game.boxArt} className='game-image'/>
                 </Box>
+                
                 <div className='col-12 col-md-8'>
-                    <div className='text-left'>
-                        <h1>Super Mario Bros.</h1>
-                        <p>Console: NES</p>
-                        <p>Release Date: September 13, 1985</p>
-                        <p>Game Price: $30</p>
-                        <p>Number of Players: 2</p>
-                        <p>Online Multiplayer: No</p>
+                    <div className="game-info">
+                        <h1>{game.title}</h1>
+                        <p><strong>Release Date:</strong> {game.releaseDate}</p>
+                        <p><strong>Game Price:</strong> ${game.gamePrice}</p>
+                        <p><strong>Number of Players:</strong> {game.numPlayers}</p>
+                        <p><strong>Online Multiplayer:</strong> {game.online ? 'True' : 'False'}</p>
+                        <p><strong>Console:</strong> {game.console}</p>
                     </div>
 
                 </div>
