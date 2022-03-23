086221e78c3f7545e7c80d0609b99818f5d5a97a
phillip-vo <philvo1991@gmail.com>
2022-03-22 20:39:33 -0500

    Implementing home page

--- src/pages/AllGames.js
+++ src/pages/AllGames.js
@@ -5,7 +5,7 @@ import { useState, useEffect } from "react";
 
 import axios from "axios";
 
-import { Link, useParams, useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function AllGames(props) {
 
