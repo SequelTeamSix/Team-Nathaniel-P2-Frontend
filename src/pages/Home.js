484d8c225ece17e7cef1021b51ec1c219a927666
phillip-vo <philvo1991@gmail.com>
2022-03-26 05:19:02 -0500

    Made homepage more responsive using bootstrap

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,10 +11,11 @@ import { Link } from "react-router-dom";
 import starsSprite from "../images/icons/5-stars-pixel.png";
 import bobombSprite from "../images/characters/bob-omb-v2.png";
 import marioSprite from "../images/characters/mario-jumping.png";
-import megamanSprite from "../images/characters/megaman-jumping.png";
+import megamanSprite from "../images/characters/megaman-shooting.png";
 import bulletSprite from "../images/characters/megaman-bullet.png";
 import megamanGame from "../images/boxarts/megaman2-box.png";
 import snakeSprite from "../images/characters/snake-reverse.png";
+import pipeSprite from "../images/misc/mario-pipe.png";
 
 import { useDispatch } from "react-redux";
 
@@ -39,106 +40,151 @@ function Home(props) {
       </div>
 
       <div className="row mt-5 d-flex align-items-center mario-rpg-section">
-        <img
-          src={require("../images/misc/mario-pipe.png")}
-          className="mario-pipe-image"
-        />
-        <Link
-          to={"/game/" + "Super Mario RPG: Legend of the Seven Stars"}
-          className="mario-rpg-image"
-        >
-          <img
-            src="https://upload.wikimedia.org/wikipedia/en/thumb/8/89/SuperMarioRPGSNESCoverArtUS.jpg/220px-SuperMarioRPGSNESCoverArtUS.jpg"
-            className="mario-boxart"
-            alt="super mario rpg"
-          />
-        </Link>
+        <div className="col-4 col-md-4 col-sm-4">
+          <img src={pipeSprite} className="img-fluid mario-pipe-image" />
+        </div>
+        <div className="col-4 col-md-4 col-sm-4">
+          <Link
+            to={"/game/" + "Super Mario RPG: Legend of the Seven Stars"}
+            className="img-fluid mario-rpg-image"
+          >
+            <img
+              src="https://upload.wikimedia.org/wikipedia/en/thumb/8/89/SuperMarioRPGSNESCoverArtUS.jpg/220px-SuperMarioRPGSNESCoverArtUS.jpg"
+              className="img-fluid mario-boxart"
+              alt="super mario rpg"
+            />
+          </Link>
+        </div>
       </div>
 
-      <div className="row d-flex justify-content-between mario-section">
-        <div className="col-6 mt-3 mario-title ">
+      <div className="row mt-5 d-flex justify-content-between mario-section">
+        <div className="col-8 col-md-8 col-sm mt-3 mario-title ">
           <h2>Super Mario RPG:</h2>
           <p>Legend of the Seven Stars</p>
-          <img src={starsSprite} width="200px" alt="5 stars" />
+          <img
+            src={starsSprite}
+            className="img-fluid"
+            width="200px"
+            alt="5 stars"
+          />
         </div>
-        <img src={marioSprite} className="mario-image" alt="mario" />
+        <div className="col-4 col-md-4 col-sm">
+          <img
+            src={marioSprite}
+            className="img-fluid mario-image"
+            width="250px"
+            alt="mario"
+          />
+        </div>
       </div>
 
-      <div className="row gap-3 justify-content-evenly align-items-end retro-deals-section">
-        <div class="d-flex justify-content-evenly align-items-center mb-5">
-          <img src={bobombSprite} className="bobomb-img" alt="bob-omb" />
-          <h2>Bob-omb Deals</h2>
+      <div className="row d-flex align-items-center bobomb-section">
+        <div className="col-3">
           <img
             src={bobombSprite}
-            className="bobomb-img img-flip"
+            className="img-fluid bobomb-img"
             alt="bob-omb"
           />
         </div>
-        <Box className="col box-deal">
-          <Link to={"/game/" + deal1}>
-            <img
-              src="https://upload.wikimedia.org/wikipedia/en/thumb/0/03/Super_Mario_Bros._box.png/220px-Super_Mario_Bros._box.png"
-              className="retro-deals-image mt-2"
-              alt="atari 2600"
-            />
-          </Link>
-          <p className="mt-3">Super Mario Bros.</p>
-          <p>$30.00</p>
-          <img src={starsSprite} className="stars-image" alt="5 stars" />
-        </Box>
-        <Box className="col box-deal">
-          <Link to={"/game/" + deal2}>
-            <img
-              src="https://upload.wikimedia.org/wikipedia/en/thumb/4/41/Legend_of_zelda_cover_%28with_cartridge%29_gold.png/220px-Legend_of_zelda_cover_%28with_cartridge%29_gold.png"
-              className="retro-deals-image mt-2"
-              alt="nes"
-            />
-          </Link>
-          <p className="mt-3">The Legend of Zelda</p>
-          <p>$30.00</p>
-          <img src={starsSprite} className="stars-image" alt="5 stars" />
-        </Box>
-        <Box className="col box-deal">
-          <Link to={"/game/" + deal3}>
-            <img
-              src="https://upload.wikimedia.org/wikipedia/en/thumb/5/57/Doom_cover_art.jpg/220px-Doom_cover_art.jpg"
-              className="retro-deals-image mt-2"
-              alt="atari 2600"
-            />
-          </Link>
-          <p className="mt-3">Doom</p>
-          <p>$25.00</p>
-          <img src={starsSprite} className="stars-image" alt="5 stars" />
-        </Box>
+        <div className="col-6">
+          <h2>Bob-omb Deals</h2>
+        </div>
+        <div className="col-3">
+          <img
+            src={bobombSprite}
+            className="img-fluid bobomb-img img-flip"
+            alt="bob-omb"
+          />
+        </div>
       </div>
 
-      <h2 className="mt-5">Ethan's Snake Game</h2>
-
-      <div className='snake-section'>
-        
-        <div className='row snake-game'>
-          <Snake />
+      <div className="row d-flex mt-5 retro-deals-section">
+        <div className="col-md col-sm">
+          <Box className="box-deal">
+            <Link to={"/game/" + deal1}>
+              <img
+                src="https://upload.wikimedia.org/wikipedia/en/thumb/0/03/Super_Mario_Bros._box.png/220px-Super_Mario_Bros._box.png"
+                className="img-fluid retro-deals-image mt-2"
+                alt="mario"
+              />
+            </Link>
+            <p className="mt-3">Super Mario Bros.</p>
+            <p>$30.00</p>
+            <img
+              src={starsSprite}
+              className="img-fluid stars-image"
+              width="200px"
+              alt="5 stars"
+            />
+          </Box>
         </div>
-        <img src={snakeSprite} />
+        <div className="col-md col-sm">
+          <Box className="box-deal">
+            <Link to={"/game/" + deal2}>
+              <img
+                src="https://upload.wikimedia.org/wikipedia/en/thumb/4/41/Legend_of_zelda_cover_%28with_cartridge%29_gold.png/220px-Legend_of_zelda_cover_%28with_cartridge%29_gold.png"
+                className="img-fluid retro-deals-image mt-2"
+               
+                alt="zelda"
+              />
+            </Link>
+            <p className="mt-3">The Legend of Zelda</p>
+            <p>$30.00</p>
+            <img
+              src={starsSprite}
+              className="img-fluid stars-image"
+              width="200px"
+              alt="5 stars"
+            />
+          </Box>
+        </div>
+        <div className="col-md col-sm" >
+          <Box className="box-deal">
+            <Link to={"/game/" + deal3}>
+              <img
+                src="https://upload.wikimedia.org/wikipedia/en/thumb/5/57/Doom_cover_art.jpg/220px-Doom_cover_art.jpg"
+                className="img-fluid mt-2 retro-deals-image"
+                alt="doom"
+              />
+            </Link>
+            <p className="mt-3">Doom</p>
+            <p>$25.00</p>
+            <img
+              src={starsSprite}
+              className="img-fluid stars-image"
+              width="200px"
+              alt="5 stars"
+            />
+          </Box>
+        </div>
         
       </div>
 
-      <div className="row d-flex justify-content-between megaman-section pb-5">
-        <div className="col-md-4 d-flex align-items-center">
-          <img src={megamanSprite} className="megaman-image" />
-          <img src={bulletSprite} className="bullet-image" />
+      <h2 className="snake-title">Ethan's Snake Game</h2>
+
+      <div className="row snake-section" >
+        <div className="row d-flex justify-content-center snake-game" style={{width: 460, height: 450}}>
+            <Snake 
+            />
         </div>
-        <div className="col-md-4 d-flex flex-column align-items-center justify-content-center">
+        <div className="col-sm">
+          <img src={snakeSprite} className="img-fluid" />
+        </div>
+      </div>
+
+      <div className="row d-flex justify-content-around align-items-center pb-5 megaman-section ">
+        <div className="col col-md-6 col-sm">
+          <img src={megamanSprite} className="img-fluid megaman-image img-flip" />
+        </div>
+        <div className="col col-md-4 col-sm">
           <MegamanBox>
             <Link to={"/game/" + "Mega Man 2"}>
-              <img src={megamanGame} className="game-image" alt="megaman 2" />
+              <img src={megamanGame} className="img-fluid game-image" alt="megaman 2" />
             </Link>
           </MegamanBox>
         </div>
       </div>
-      <div className='row'>
-
-      </div>
+      <div className="row"></div>
     </div>
   );
 }
